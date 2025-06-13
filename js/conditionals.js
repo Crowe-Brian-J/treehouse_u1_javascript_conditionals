2e519db2f04e1face0a857ea9d9ea22c6c44f4b1
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-13 14:48:19 -0400

    add notes

--- js/conditionals.js
+++ js/conditionals.js
@@ -2,10 +2,24 @@ const answer = prompt('Which planet is closest to the sun?')
 
 //Two doors in "if...else" statement, true or false (boolean)
 
-//Video said to use .toUpperCase()
+//Video said to use .toUpperCase(), to show masery, I did .toLowerCase()
 
 if (answer.toLowerCase() === 'mercury') {
   console.log("That's correct!")
 } else {
   console.log("Sorry, that's incorrect.")
 }
+
+//Double Equal vs Triple Equal
+/*
+  '3' == 3 (true) --> Different data types, same value
+  '3' === 3 (false) --> Different data types (strict equality)
+*/
+
+//Not Equal To
+/*
+  10 !== 9          (true)
+  '10' !== 10       (true)  (strict)
+  '10' != 10        (false) (not strict)
+  'Java' !== 'java' (true)
+*/
