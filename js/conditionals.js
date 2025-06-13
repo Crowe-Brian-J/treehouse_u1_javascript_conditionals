3932c7352ac9bd098554901f65bf4b841da27fb6
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-13 14:39:23 -0400

    modify conditionals.js file

---
+++ js/conditionals.js
@@ -0,0 +1,11 @@
+const answer = prompt('Which planet is closest to the sun?')
+
+//Two doors in "if...else" statement, true or false (boolean)
+
+//Video said to use .toUpperCase()
+
+if (answer.toLowerCase() === 'mercury') {
+  console.log("That's correct!")
+} else {
+  console.log("Sorry, that's incorrect.")
+}
